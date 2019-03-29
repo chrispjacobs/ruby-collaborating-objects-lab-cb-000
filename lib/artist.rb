3dba33d3f7e315a7cae06e4f25773c2c9b2f3555
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-29 23:17:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,3 @@
-require_relative 'mp3_importer.rb'
-require_relative 'song.rb'
-
 class Artist
     @@all = []
     attr_accessor :name
@@ -41,11 +38,3 @@ class Artist
         puts stringified_array
     end
 end
-
-
-new_artist = Artist.find_or_create_by_name("Michael Jackson")
-dirty_diana = Song.new("Dirty Diana")
-billie_jean = Song.new("Billie Jean")
-new_artist.add_song(dirty_diana)
-new_artist.add_song(billie_jean)
-new_artist.print_songs
