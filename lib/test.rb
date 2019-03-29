2aabbca0879e86e00534f38a5402471c8a74cca9
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-29 23:17:31 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -1,3 +1,11 @@
 require_relative 'mp3_importer.rb'
 require_relative 'song.rb'
 require_relative 'artist.rb'
+
+
+new_artist = Artist.find_or_create_by_name("Michael Jackson")
+dirty_diana = Song.new("Dirty Diana")
+billie_jean = Song.new("Billie Jean")
+new_artist.add_song(dirty_diana)
+new_artist.add_song(billie_jean)
+new_artist.print_songs
